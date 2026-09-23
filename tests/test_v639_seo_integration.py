ef761651a81454629d25530d8eef89fe859b2056
92dugenet-ctrl <92dugenet@gmail.com>
2026-09-23 17:36:05 +0200

    test: update sitemap surface for final SEO network

--- tests/test_v639_seo_integration.py
+++ tests/test_v639_seo_integration.py
@@ -25,7 +25,7 @@ def test_v639_structured_data_and_faq_are_visible():
 
 def test_v639_sitemap_and_robots():
     sm=sitemap().decode(); rb=robots().decode()
-    assert sm.count('<url>') == 67
+    assert sm.count('<url>') == 69
     assert 'sitemap.xml' in rb
     assert 'Disallow: /app' in rb
     assert 'Disallow: /v1/' in rb
